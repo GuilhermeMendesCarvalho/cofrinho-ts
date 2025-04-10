72d2fd573ee35bf914da5002dba883b2a8fa37df
GuiMendes <guilhermemendescarvalho1@gmail.com>
2025-04-10 19:25:21 -0300

    corrigido queryselector

--- js/cofrinho.js
+++ js/cofrinho.js
@@ -1,4 +1,7 @@
-let saldo = 5000;
+let saldo = 10000;
 
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
+const elementoSaldo = document.querySelector('.saldo-valor .valor');
+elementoSaldo.textContent = saldo.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+});
