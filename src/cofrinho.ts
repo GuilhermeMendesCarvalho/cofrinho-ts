9449acf260acc66dbc22f9684d069c861d746854
GuiMendes <guilhermemendescarvalho1@gmail.com>
2025-04-11 18:05:05 -0300

    limpeza e reestruturação do código

--- src/cofrinho.ts
+++ src/cofrinho.ts
@@ -1,5 +1,6 @@
 let saldo = 10000;
 
+alert('testando compilador!!');
 const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement;
 if (elementoSaldo) {
   elementoSaldo.textContent = saldo.toLocaleString('pt-BR', {
